a7fd446fade76058a7be74f00406fe9c547a8d03
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-08 21:16:51 +0200

    added necessary annotation and the missing dependency from spring initializr

--- eurekaserver/src/main/java/com/farzan/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/farzan/eurekaserver/EurekaserverApplication.java
@@ -2,8 +2,10 @@ package com.farzan.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
