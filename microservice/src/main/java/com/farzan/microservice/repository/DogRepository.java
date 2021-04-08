9361349ac7acee9c6f3a9a0f923627df49aa8554
farahmadii <33867973+farahmadii@users.noreply.github.com>
2021-04-08 21:32:19 +0200

    implemented dog entity and repository

---
+++ microservice/src/main/java/com/farzan/microservice/repository/DogRepository.java
@@ -0,0 +1,9 @@
+package com.farzan.microservice.repository;
+
+import entity.Dog;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
